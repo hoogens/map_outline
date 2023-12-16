d06014224e7699094d966b54a13c743e1091fa19
hoogens <sarahbhgoes@gmail.com>
2023-12-16 01:41:44 +0100

    Changed landfile to geopackage file and added an alignment function

--- map_outline.py
+++ map_outline.py
@@ -3,25 +3,42 @@ Script to create an outline of a land 'The Netherlands' and river 'Rhine'
 @author: Sarah H
 """
 import geopandas as gpd
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import numpy
+import os
 
 def load_netherlands_map():
     """
     Function to load the map of the Netherlands
     """
-    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-    nl = world[world['name'] == 'Netherlands']
-    return nl
+    netherlands = gpd.read_file('gadm36_NLD.gpkg') #'provinces.geojson'
+    return netherlands
 
 def load_rhine_map():
     """"
     Function to load the river Rhine
     """
-    rivers = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
-    rhine = rivers[rivers['name'] == 'Rhein']
+    rivers_path = gpd.datasets.get_path("naturalearth_lowres")
+    rivers = gpd.read_file(rivers_path)
+    rhine = rivers[(rivers['name'] == 'Rhein') & (rivers['iso_a3'] == 'NLD')]
     return rhine
 
+def align_land_and_river():
+    """
+    Function to align landmap and river
+    """
+    netherlands = load_netherlands_map()
+    rhine = load_rhine_map()
 
+    ax = netherlands.plot(color='lightgray', edgecolor='black')
 
+    # rhine.plot(ax=ax, color='blue', linewidth=2)
 
-# if __name__ == '__main__':
+    ax.set_aspect('equal')
+
+    plt.savefig('map_and_river_align.png')
+
+    plt.show()
+
+if __name__ == '__main__':
+    align_land_and_river()

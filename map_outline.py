fa85b429f039ca21cdfa3ddc4f6248815629b507
hoogens <sarahbhgoes@gmail.com>
2023-12-16 01:06:35 +0100

    Creation function to load the river Rhine

--- map_outline.py
+++ map_outline.py
@@ -6,6 +6,17 @@ import geopandas as gpd
 import matplotlib as plt
 
 def netherlands_map():
+    """
+    Function to load the map of the Netherlands
+    """
     world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
     nl = world[world['name'] == 'Netherlands']
     return nl
+
+def rhine_map():
+    """"
+    Function to load the river Rhine
+    """
+
+
+# if __name__ == '__main__':
